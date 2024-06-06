1dc5f4e77a270bff42054c06de76d7dbf609de7e
Gideon Ladiyo <alwaysfun0604@gmail.com>
2024-06-06 13:52:32 +0700

    perbaiki modular

--- admin/page/page.php
+++ admin/page/page.php
@@ -12,8 +12,26 @@ if($_GET['mod'] == "dashboard"){
     include "berita/berita.php";
 } elseif($_GET['mod'] == "ulasan"){
     include "ulasan/ulasan.php";
+    if ($action == "tambah") {
+        include "ulasan/tambah.php";
+    } elseif ($action == "edit") {
+        include "ulasan/edit.php";
+    } elseif ($action == "delete") {
+        include "ulasan/hapus.php";
+    } elseif ($action == "data") {
+        include "ulasan/datatabel.php";
+    }
 } elseif($_GET['mod'] == "fasilitas"){
     include "fasilitas/fasilitas.php";
+    if ($action == "tambah") {
+        include "fasilitas/tambah.php";
+    } elseif ($action == "edit") {
+        include "fasilitas/edit.php";
+    } elseif ($action == "delete") {
+        include "fasilitas/hapus.php";
+    } elseif ($action == "data") {
+        include "fasilitas/datatabel.php";
+    }
 } elseif($_GET['mod'] == "kontak"){
     include "kontak/kontak.php";
 }
