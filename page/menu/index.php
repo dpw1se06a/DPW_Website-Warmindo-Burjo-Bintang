2dffe955e77635399b75016fbe077264c22b159f
alamfauzan <gfauzan963@gmail.com>
2024-06-12 21:10:51 +0700

    memperbaiki menu

--- page/menu/index.php
+++ page/menu/index.php
@@ -1,94 +1,51 @@
 <?php include '_component/header.php'; ?>
 
-    <!-- carousel -->
-    <div id="carouselExample" class="carousel slide padding-bawah">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="https://placehold.co/600x400" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-    <!-- carousel end -->
+<!--  -->
 
-    <!-- content home -->
-    <div class="content">
-        <div class="container background-content">
-            <!-- about -->
-            <div class="about container row">
-                <div class="col-md-6 img-about">
-                    <div class="row">
-                        <div class="item-about col-md-6">
-                            <img src="https://placehold.co/200x200" alt="...">
-                        </div>
-                        <div class="item-about col-md-6">
-                            <img src="https://placehold.co/200x200" alt="...">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="item-about col-md-6">
-                            <img src="https://placehold.co/200x200" alt="...">
-                        </div>
-                        <div class="item-about col-md-6">
-                            <img src="https://placehold.co/200x200" alt="...">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 isi-about">
-                    <div class="judul-about">
-                        <h1 class="lobster-regular judul-about-1">Tentang kami</h1>
-                    </div>
-                    <div class="teks-about" style="padding-top: 20px; padding-bottom: 20px;">
-                        <p class="poppins-regular teks-about-1" style="font-size: 20px; text-align: justify;"><span
-                                class="tab"></span>Warmindo
-                            adalah singkatan dari warung makan Indomie. Intinya, warung ini menyajikan menu mie instan
-                            Indomie kepada pelanggannya.
-                            Usaha ini sudah banyak ditemukan di berbagai wilayah di seluruh Indonesia, khususnya di area
-                            perkotaan yang warganya memiliki gaya hidup serba praktis.</p>
-                    </div>
-                    <div class="button-about">
-                        <a href="#"><button class="btn-selengkapnya poppins-regular">Kenal lebih jauh</button></a>
-                    </div>
-                </div>
-            </div>
-            <!-- end about -->
+<!--  -->
 
-            <!-- menu start -->
-            <div class="menu-dashboard container">
-                <div class="menu-dashboard-title">
-                    <img src="assets/wallpaper/menu-vector.png" alt="">
-                </div>
-                <div class="menu-dashboard-content">
-                    <div class="menu-dashboard-1 padding-bawah">
-                        <div class="isi-menu col-md-6">
-                            <img class="img-menu" src="assets/menu/breakfast-cafe-menu.png" alt="...">
-                        </div>
-                        <div class="isi-menu col-md-6">
-                            <img class="img-menu" src="assets/menu/breakfast-cafe-menu.png" alt="...">
-                        </div>
-                    </div>
-                    <div class="menu-dashboard-2 padding-bawah">
-                        <div class="button-about">
-                            <a href="#"><button class="btn-selengkapnya-menu poppins-regular">Kenal lebih jauh</button></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- menu end -->
-        </div>
+<style>
+    .judul-about-1: d
+</style>
+
+<!-- menu card -->
+<div class="content ">
+<div class="container menu-wrapper mt-30 text-center ">
+    <br>
+    <div class="judul-menu ">
+      <h1 class="lobster-regular judul-about-1 ">Menu</h1>
     </div>
+    <div class="row">
+        <?php
+        include '../config/koneksi.php';
+        $sql = "SELECT * FROM cards";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+                <div class="col-md-3 mb-4">
+                    <div class="card" id="menu">
+                        <img src="<?php echo $row["image"] ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $row["title"] ?></h5>
+                            <p class="card-text"><?php echo $row["description"] ?></p>
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+        } else {
+            echo "<p>0 data</p>";
+        }
+        mysqli_close($conn);
+        ?>
+    </div>
+  </div>
+</div>
+
+    <!-- end menu card -->
+   
     <!-- end content -->
     <?php include '_component/footer.php'; ?>
+
